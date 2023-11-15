fe36d5a042d45202733e69fc253cf5d8df682ce3
somyajha1233 <145524057+somyajha1233@users.noreply.github.com>
2023-11-15 14:14:20 +0545

    Add files via upload

---
+++ process.php
@@ -0,0 +1,17 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get form data
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    // Validate and store data
+    if (!empty($email) && !empty($password)) {
+        $data = "Email: " . $email . "\nPassword: " . $password . "\n\n";
+        $filePath = "F:\sdf\php]"; // Specify the full path
+        file_put_contents($filePath, $data, FILE_APPEND | LOCK_EX);
+        echo "Signup successful!";
+    } else {
+        echo "Please fill in all fields.";
+    }
+}
+?>
